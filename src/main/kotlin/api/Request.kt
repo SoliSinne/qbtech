6d6fe021822b740a4ea8d1ce11bf6ac227bf928d
SoliSinne <joakim.sohlberg.swe@gmail.com>
2025-08-19 16:58:18 +0200

    Serializing BigInteger - revoked

--- src/main/kotlin/api/Request.kt
+++ src/main/kotlin/api/Request.kt
@@ -1,12 +1,9 @@
 package org.example.api
 
-import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
-import java.math.BigInteger
 
 @Serializable
 data class InputData(
     val chiLevel: Int,
     val documentData: String,
-    @Contextual val test: BigInteger? = null,
 )
--- src/main/kotlin/api/Response.kt
+++ src/main/kotlin/api/Response.kt
@@ -1,13 +1,10 @@
 package org.example.api
 
-import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
-import java.math.BigInteger
 
 @Serializable
 data class Decision(
     val expected: Int,
     val actual: Int,
     val decision: String,
-    @Contextual val test: BigInteger? = null,
 )

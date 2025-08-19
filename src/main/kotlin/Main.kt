c11b3644fa10133efe363758c4a6f6a34549f2ab
SoliSinne <joakim.sohlberg.swe@gmail.com>
2025-08-19 16:55:54 +0200

    Serializing BigInteger

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,11 +7,16 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
+import org.example.api.Decision
 import org.example.api.InputData
 
 fun main() {
     embeddedServer(Netty, 8080) {
         routing {
+            get("/decision") {
+                val decision = Decision(1, 0, "Not able to give a proper decision.")
+                call.respondText(decision.toString(), ContentType.Text.Plain, HttpStatusCode.OK)
+            }
             post("/check") {
                 val inputData = Json.decodeFromString<InputData>(call.receiveText())
                 val decision = theChiSquareTestLib(inputData)
